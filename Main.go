91842392938d54e99446fc3f08b1d1af07641049
Filip-Pajalic <filip.pajalic@gmail.com>
2021-06-13 11:24:45 +0200

    Modified slice of slices to draw tilemap, need to do some testing on if slice of slice vs a struct for the array/table type, what is best practice

--- Main.go
+++ Main.go
@@ -13,24 +13,58 @@ const (
 	level = 1
 )
 
+type TileTypeAlias int
+type TileType struct{
+	TileEmpty TileTypeAlias
+	TileSolid TileTypeAlias
+}
+
+var TileTypeEnum = &TileType{
+	TileEmpty: 0,
+	TileSolid: 1, 
+}
+
+var TileSolid = 1
+var TileEmpty = 0
 var size_w int = 0
 var size_h int = 0
-var map_m string = ""
+var genMap [][]int32
+
+//perhaps make struct for 2d array for tiles?
+
+func newLevel(x int , y int){	
+	fmt.Println(x)
+	for i := 0; i < y+2; i++ {
+		m := []int32{}
+		for j := 0; j < x+3; j++ {
+			m = append(m, int32(TileTypeEnum.TileEmpty))
+		}
+		genMap = append(genMap, m)
+	}
+	
+}
 
 func createLevel(L *lua.LState) int{
 	size_w = L.ToInt(1)
 	size_h = L.ToInt(2)
+
+	newLevel(size_w,size_h)
 	return 1
 }
 
 func setTile(L *lua.LState) int{
+	y := L.ToInt(2)
+	print("Y:"+ fmt.Sprint(y))
+	x := L.ToInt(1)
+	println(", X:"+ fmt.Sprint(x))
+	
+	
 	switch tile := L.ToInt(3); tile {
+	case 0:
+		genMap[x][y] = int32(TileTypeEnum.TileEmpty)
 	case 1:
-
-	case 2:
-
+		genMap[x][y] = int32(TileTypeEnum.TileSolid)
 	}
-	map_m = L.ToString(1)
 	return 1
 }
 
@@ -55,7 +89,6 @@ func main() {
 		}, lua.LNumber(level)); err!= nil {
 		panic(err)					
 	}
-	print(fmt.Sprint(size_w) + " " + fmt.Sprint(size_h))
 
 	window, err := sdl.CreateWindow("GameWindow", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		screenHeight*int32(size_h), screenWidth*int32(size_w), sdl.WINDOW_SHOWN)
@@ -70,17 +103,25 @@ func main() {
 	}
 	surface.FillRect(nil, 0)
 
-	rect := sdl.Rect{0, 0, screenHeight, screenWidth}
-	surface.FillRect(&rect, 0xffff0000)
-	window.UpdateSurface()
+	for i := range genMap{
+		for j := range genMap[i]{
+			if genMap[i][j] == int32(TileTypeEnum.TileSolid){
+				rect := sdl.Rect{int32(screenWidth*i), int32(screenHeight*j), screenHeight, screenWidth}
+				surface.FillRect(&rect, 0xffff0000)
+			} else{
+				rect := sdl.Rect{int32(screenWidth*i), int32(screenHeight*j), screenHeight, screenWidth}
+				surface.FillRect(&rect, 0x11ff88aa)
+			}
+		}
 
+	}
+	window.UpdateSurface()
 	
 	running := true
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.QuitEvent:
-				
 				println("Quit")
 				running = false
 			}
